a06fae5298a7884f11af7651c836231a82b27550
HRISHITA SAMIR MAVANI <hr637632@dal.ca>
2022-02-19 15:55:46 -0400

    Added Abstract Methods to JobRoleDao - added abstract methods to JobRoleDao.java

--- src/main/java/com/scheduler/app/model/dao/JobRoleDao.java
+++ src/main/java/com/scheduler/app/model/dao/JobRoleDao.java
@@ -1,4 +1,15 @@
 package com.scheduler.app.model.dao;
 
+import com.scheduler.app.model.entity.JobRolePojo;
+
+import java.util.List;
+
 public interface JobRoleDao {
+    List<JobRolePojo> selectAllRecords();
+    List<JobRolePojo> selectByTitleJobTitle();
+    List<JobRolePojo> selectByDepartment();
+
+    boolean insertJobRole(JobRolePojo jobRole);
+    boolean updateJobRole(JobRolePojo jobRole);
+    boolean deleteJobRole(JobRolePojo jobRole);
 }
