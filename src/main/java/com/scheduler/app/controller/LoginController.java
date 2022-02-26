3c2e14f2279a260f4470f6af4f828d70c9e2874c
Ankush Mudgal <an564394@dal.ca>
2022-02-26 16:20:31 -0400

    Updated the LoginController & login.js - Updated the controller to add Consumes and Produces Notations. - Modified the login.js to handle response from DB. - Unit tested in local: Passed through UI & Postman.

--- src/main/java/com/scheduler/app/controller/LoginController.java
+++ src/main/java/com/scheduler/app/controller/LoginController.java
@@ -5,6 +5,9 @@ import com.scheduler.app.model.response.LoginResponse;
 import com.scheduler.app.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @RestController
 @RequestMapping("/login")
@@ -15,10 +18,11 @@ public class LoginController {
 
 
     @PostMapping("/request")
-    public LoginResponse inputCredentials(@RequestBody LoginRequest loginRequest){
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public @ResponseBody LoginResponse inputCredentials(@RequestBody LoginRequest loginRequest){
 
-        LoginResponse loginResponse = loginService.inputCredentials(loginRequest);
-        return loginResponse;
+        return loginService.inputCredentials(loginRequest);
     }
 
 
