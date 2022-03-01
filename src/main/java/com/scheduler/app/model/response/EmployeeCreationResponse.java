4b479d6b1a85658dd37d669b6d37248a97cf511f
Ankush Mudgal <an564394@dal.ca>
2022-03-01 18:08:17 -0400

    Updates to the Holiday Creation API Code

--- src/main/java/com/scheduler/app/model/response/EmployeeCreationResponse.java
+++ src/main/java/com/scheduler/app/model/response/EmployeeCreationResponse.java
@@ -1,13 +1,15 @@
 package com.scheduler.app.model.response;
 
 import com.scheduler.app.constants.REQUEST_STATUS;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class EmployeeCreationResponse {
 
     private REQUEST_STATUS status;
-    private boolean isCreated;
+    private boolean empCreated;
 }
--- src/main/java/com/scheduler/app/service/EmployeeCreationServiceImpl.java
+++ src/main/java/com/scheduler/app/service/EmployeeCreationServiceImpl.java
@@ -1,17 +1,50 @@
 package com.scheduler.app.service;
 
+import com.scheduler.app.constants.REQUEST_STATUS;
+import com.scheduler.app.model.dto.EmployeeCredsDTO;
+import com.scheduler.app.model.entity.EmpDetailPOJO;
+import com.scheduler.app.model.repo.EmpDetailRepository;
 import com.scheduler.app.model.request.EmployeeCreationRequest;
 import com.scheduler.app.model.request.HolidayCreationRequest;
 import com.scheduler.app.model.response.EmployeeCreationResponse;
 import com.scheduler.app.model.response.HolidayCreationResponse;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeCreationServiceImpl implements EmployeeCreationService{
 
+    @Autowired
+    EmpDetailRepository empDetailRepository;
+
     @Override
     public EmployeeCreationResponse createNewEmployee(EmployeeCreationRequest employeeCreationRequest) {
-        return null;
+
+        EmployeeCreationResponse employeeCreationResponse = new EmployeeCreationResponse(REQUEST_STATUS.FAILED,false);
+        EmpDetailPOJO newEmployee = new EmpDetailPOJO();
+
+        if(employeeCreationRequest != null && Strings.isNotBlank(employeeCreationRequest.getEmployeeNumber())){
+
+            EmployeeCredsDTO emp = empDetailRepository.getDistinctFirstByEmployeeNumber(employeeCreationRequest.getEmployeeNumber());
+            if(emp != null && Strings.isNotBlank(emp.getEmployeeNumber())){
+
+                newEmployee.setId(null);
+                newEmployee.setEmployeeNumber(employeeCreationRequest.getEmployeeNumber());
+                newEmployee.setEmailId(employeeCreationRequest.getEmailId());
+                newEmployee.setDateOfJoining(employeeCreationRequest.getDateOfJoining());
+                newEmployee.setDepartmentId(employeeCreationRequest.getDepartmentId());
+                newEmployee.setFirstName(employeeCreationRequest.getFirstName());
+                //.
+                //.
+                //.
+                //.
+
+            }
+
+        }
+        return employeeCreationResponse;
+
     }
 
 }
