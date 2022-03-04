95f1aa5102d7aac84fa80195fc5f8dfad75a7733
Ankush Mudgal <an564394@dal.ca>
2022-03-04 19:16:33 -0400

    Added Maven Profiles in the Project. - Dev is for DEV DB Connection. - Test for test & PROD for prod. - Default is DEV.

--- src/main/java/com/scheduler/app/model/repo/HolidayRepo.java
+++ src/main/java/com/scheduler/app/model/repo/HolidayRepo.java
@@ -4,9 +4,11 @@ import com.scheduler.app.model.dto.EmployeeCredsDTO;
 import com.scheduler.app.model.entity.EmpDetailPOJO;
 import com.scheduler.app.model.entity.HolidayPOJO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+@Repository
 public interface HolidayRepo extends JpaRepository<HolidayPOJO, Integer> {
 
 
