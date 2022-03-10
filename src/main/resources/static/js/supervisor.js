0fdbcc19a47b8b207c2bf84e2bc8e9c7718575e6
Bhavesh Lalwani <bhavesh.lalwani@dal.ca>
2022-03-10 18:29:57 -0400

    Adding dynamic fields for employee details

--- src/main/resources/static/js/supervisor.js
+++ src/main/resources/static/js/supervisor.js
@@ -1,8 +1,15 @@
+$(document).ready(function(){
+    $( "#department-type" ).hide();
+    $( "#employee-detail" ).hide();
+});
+
 $('#supervisor-date-picker').datepicker( {
     showOtherMonths: true,
     selectOtherMonths: true,
     multiselect: true,
     onSelect: function(selectedDate, instance) {
+        $( "#department-type" ).show();
+        $( "#employee-detail" ).show();
         console.log(selectedDate);
         var date = new Date(selectedDate);
         var weekStart = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
@@ -12,3 +19,28 @@ $('#supervisor-date-picker').datepicker( {
         console.log("weekend", weekend);
     }
 });
+
+$( "#department" ).change(function() {
+    $( "#employee-detail" ).show();
+});
+
+$( "#add-emp-details").click(function() {
+    var html = '';
+    html += '<div class="row">';
+    html += '    <div class="form-group col-md-6">';
+    html += '        <label for="employee-type-1">Employee role:</label>';
+    html += '        <select class="form-control" id="employee-type-1">';
+    html += '            <option>Role 1</option>';
+    html += '            <option>Role 2</option>';
+    html += '        </select>';
+    html += '    </div>';
+    html += '    <div class="form-group col-md-6">';
+    html += '        <label for="employeesCount-1">Number of Employees:</label>';
+    html += '        <input type="number" class="form-control" id="employeesCount-1">';
+    html += '    </div>';
+    html += '</div>';
+
+    $('#dynamic-emp').prepend(html);
+});
+
+
