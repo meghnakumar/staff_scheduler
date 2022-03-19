dd48e07e58ce960f53ddf1ac45333e7b25249b8d
Meghna Kumar <mg446605@dal.ca>
2022-03-19 15:47:44 -0300

    Sprint 4| AG2SS-45 fixed StaffSchedulerApplicationTests

--- src/test/java/StaffSchedulerApplicationTests.java
+++ src/test/java/StaffSchedulerApplicationTests.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
 
-@SpringBootTest
+@SpringBootConfiguration
 class StaffSchedulerApplicationTests {
 
 	@Test
