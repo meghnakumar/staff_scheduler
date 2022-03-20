1c7d6bdad4a06a6bc8088e3d812a4d07425e8523
Meghna Kumar <mg446605@dal.ca>
2022-03-19 21:13:24 -0300

    Sprint 4| AG2SS-46 swagger documentation for all the APIs

--- src/main/java/com/scheduler/app/controller/AdminController.java
+++ src/main/java/com/scheduler/app/controller/AdminController.java
@@ -6,6 +6,11 @@ import com.scheduler.app.model.response.EmployeeCreationResponse;
 import com.scheduler.app.model.response.HolidayCreationResponse;
 import com.scheduler.app.service.EmployeeCreationService;
 import com.scheduler.app.service.HolidayCreationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +29,15 @@ public class AdminController {
     @Autowired
     HolidayCreationService holidayCreationService;
 
-
+    @Operation(summary = "Register the employees and store the data to DB")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee is successfully created and added to DB",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeCreationResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found",
+                    content = @Content)})
     @PostMapping("/create/employee")
     @Consumes(value = MediaType.APPLICATION_JSON)
     @Produces(value = MediaType.APPLICATION_JSON)
@@ -34,6 +47,15 @@ public class AdminController {
         return employeeCreationService.createNewEmployee(employeeCreationRequest);
     }
 
+    @Operation(summary = "Store the holidays declared by admin to holiday table in DB")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Holiday is successfully created and added to DB",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = HolidayCreationResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found",
+                    content = @Content)})
     @PostMapping("/create/holiday")
     @Consumes(value = MediaType.APPLICATION_JSON)
     @Produces(value = MediaType.APPLICATION_JSON)
--- src/main/java/com/scheduler/app/controller/LoginController.java
+++ src/main/java/com/scheduler/app/controller/LoginController.java
@@ -3,6 +3,11 @@ package com.scheduler.app.controller;
 import com.scheduler.app.model.request.LoginRequest;
 import com.scheduler.app.model.response.LoginResponse;
 import com.scheduler.app.service.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.ws.rs.Consumes;
@@ -16,7 +21,16 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
-
+    @Operation(summary = "Validate the loginId and Password, and return the type of user to land on " +
+            "on the desired landing page.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Correct user type is returned based on the request",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = LoginResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found",
+                    content = @Content)})
     @PostMapping("/request")
     @Consumes(value = MediaType.APPLICATION_JSON)
     @Produces(value = MediaType.APPLICATION_JSON)
