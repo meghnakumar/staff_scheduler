ec7a8e058390fc8ca68ec9cdcf90231cefac3b0d
Ankush Mudgal <an564394@dal.ca>
2022-03-27 16:12:21 -0300

    Sprint 4| AG2SS-48: Internal Documentation for EmployeeCreationService.

--- src/main/java/com/scheduler/app/service/EmployeeCreationServiceImpl.java
+++ src/main/java/com/scheduler/app/service/EmployeeCreationServiceImpl.java
@@ -12,30 +12,51 @@ import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Employee Creation Service.
+ * This Service helps in the creation & registration of a new Employee in the System.
+ * All the details for the new Employee are received form the front-end.
+ */
 @Service
 public class EmployeeCreationServiceImpl implements EmployeeCreationService {
 
+    /**
+     * Auto-wired Component : JPA Repo to the Emp Details Table.
+     */
     @Autowired
     EmpDetailRepository empDetailRepository;
 
+    /**
+     * Auto-wired Component : Utility Service to Send Email sto Newly Created Employees.
+     */
     @Autowired
     MailService mailService;
 
+    /**
+     * Create new Employee, and return the status of creation via the EmployeeCreationResponse object.
+     *
+     * @param employeeCreationRequest the employee creation request, with fields for creating the new employee.
+     * @return the employee creation response type, with status.
+     */
     @Override
     public EmployeeCreationResponse createNewEmployee(EmployeeCreationRequest employeeCreationRequest) {
 
         EmployeeCreationResponse employeeCreationResponse = new EmployeeCreationResponse(REQUEST_STATUS.FAILED, false);
         EmpDetailPOJO newEmployee = new EmpDetailPOJO();
 
+        //Check if the Request object is not null & has the mandatory filed Employee Number
         if (employeeCreationRequest != null && Strings.isNotBlank(employeeCreationRequest.getEmployeeNumber())) {
 
+            //Check if an Employee with the same Employee Number already exists in the system.
             EmployeeCredsDTO emp = empDetailRepository.getDistinctFirstByEmployeeNumber(employeeCreationRequest.getEmployeeNumber());
             if (emp == null && Strings.isNotBlank(employeeCreationRequest.getEmployeeNumber())) {
 
-                // To generate a random password
+                // To generate a random password for first time login
+                //Will be sent to the new Employee via an email.
                 RandomTextGeneratorUtil passwordGenerator = new RandomTextGeneratorUtil();
                 String generatedPassword = passwordGenerator.generateCommonTextPassword();
 
+                //Create a new Employee with request values.
                 newEmployee.setId(null);
                 newEmployee.setEmployeeNumber(employeeCreationRequest.getEmployeeNumber());
                 newEmployee.setEmailId(employeeCreationRequest.getEmailId());
@@ -49,9 +70,13 @@ public class EmployeeCreationServiceImpl implements EmployeeCreationService {
                 newEmployee.setRoleId(employeeCreationRequest.getRoleId());
                 newEmployee.setSinNumber(employeeCreationRequest.getSinNumber());
                 newEmployee.setPhoto(employeeCreationRequest.getPhoto());
+                //Set one-time password.
                 newEmployee.setLoginPassword(generatedPassword);
+                //Save the Employee in the DB.
                 EmpDetailPOJO employee = empDetailRepository.saveAndFlush(newEmployee);
+
                 if (employee != null) {
+                    //Successful Response.
                     employeeCreationResponse.setStatus(REQUEST_STATUS.SUCCESS);
                     employeeCreationResponse.setCreated(true);
                     // to initiate mail to share credentials to newly created employee
@@ -60,14 +85,16 @@ public class EmployeeCreationServiceImpl implements EmployeeCreationService {
                     mailService.sendMailToEmployee(employee, subject, message);
                     return employeeCreationResponse;
                 }
+
+                //Response in case the Employee with the same number already exists.
             } else if (emp != null && emp.getEmployeeNumber().equals(employeeCreationRequest.getEmployeeNumber())) {
 
                 employeeCreationResponse.setStatus(REQUEST_STATUS.INVALID_REQUEST);
                 employeeCreationResponse.setCreated(false);
-
             }
         }
 
+        //Return EmployeeCreationResponse
         return employeeCreationResponse;
 
     }
