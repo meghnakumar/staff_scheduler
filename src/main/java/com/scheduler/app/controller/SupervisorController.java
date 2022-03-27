e9b0ec7773b11babe37c1ea66b06d97fb46d2d43
Bhavesh Lalwani <bhavesh.lalwani@dal.ca>
2022-03-26 22:03:41 -0300

    Adding /supervisor/save/shifts api to store shift details

--- src/main/java/com/scheduler/app/controller/SupervisorController.java
+++ src/main/java/com/scheduler/app/controller/SupervisorController.java
@@ -3,8 +3,18 @@ package com.scheduler.app.controller;
 import com.scheduler.app.model.entity.DailyShiftPOJO;
 import com.scheduler.app.model.entity.ScheduleDetails;
 import com.scheduler.app.model.request.ScheduleRequest;
+import com.scheduler.app.model.request.ShiftDetailsRequest;
+import com.scheduler.app.model.request.StaffAvailabilitiesRequest;
+import com.scheduler.app.model.request.StaffAvailabilityRequest;
 import com.scheduler.app.model.response.ScheduleResponse;
+import com.scheduler.app.model.response.ShiftDetailsResponse;
+import com.scheduler.app.model.response.StaffAvailabilityResponse;
 import com.scheduler.app.service.SchedulerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,15 +23,32 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import java.sql.Date;
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
-@RequestMapping("/schedule")
+@RequestMapping("/supervisor")
 public class SupervisorController {
 
     @Autowired
     SchedulerService schedulerService;
+
+    @Operation(summary = "Store the shift details of staff for the next week in DB")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Provided shift is successfully added" +
+                    "to DB",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShiftDetailsResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found",
+                    content = @Content)})
+    @PostMapping("/save/shifts")
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public @ResponseBody
+    ShiftDetailsResponse shiftDetails(@RequestBody ShiftDetailsRequest shiftDetailsRequest) {
+        return schedulerService.saveShiftDetails(shiftDetailsRequest);
+    }
 
     @GetMapping("/get-schedule")
     @Produces(value = MediaType.APPLICATION_JSON)
