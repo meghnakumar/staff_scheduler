4a061e2e52eec4baf89b5d2d998abc518ddfa6b6
HRISHITA SAMIR MAVANI <hr637632@dal.ca>
2022-04-05 14:44:22 -0300

    Algorithm bug fix

--- src/main/java/com/scheduler/app/staff/model/response/EmployeeDetailsResponse.java
+++ src/main/java/com/scheduler/app/staff/model/response/EmployeeDetailsResponse.java
@@ -1,7 +1,7 @@
 package com.scheduler.app.staff.model.response;
 
 import com.scheduler.app.constants.REQUEST_STATUS;
-import com.scheduler.app.admin.model.dto.EmployeeDetailDTO;
+import com.scheduler.app.staff.model.dto.EmployeeDetailDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
